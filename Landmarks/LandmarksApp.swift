8d2f3f26442666b03a34ff2d9bdad936019fb3ad
Rebecca Stone <rebecca@qawolf.com>
2025-06-05 17:09:42 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rebecca Stone on 6/5/25.
+
+// An app that uses the SwiftUI app life cycle has a structure that conforms to the App protocol.
+// The structure’s body property returns one or more scenes, which in turn provide content for display.
+// The @main attribute identifies the app’s entry point.
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
