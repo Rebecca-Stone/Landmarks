6d2e54b78af35605848e648f29a1d43dea53eccc
Rebecca Stone <rebecca@qawolf.com>
2025-06-06 13:32:09 -0500

    Making progress on the badge

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Rebecca Stone on 6/6/25.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
